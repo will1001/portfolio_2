8efdfc8c9b9cf73bb357a1626fd39a59ec64aa62
wili1001 <wilirahmatm@gmail.com>
2020-03-18 13:04:25 +0800

    Updates

---
+++ precache-manifest.4cbebd4bb76cc48f14b79213665835f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce5afc8f3019a45813f51f31b750b5c",
+    "url": "/portfolio_2/index.html"
+  },
+  {
+    "revision": "90fcd88dc1116058f82e",
+    "url": "/portfolio_2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5fc8c9bbb221b5b9922b",
+    "url": "/portfolio_2/static/js/2.eeb86ee0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio_2/static/js/2.eeb86ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90fcd88dc1116058f82e",
+    "url": "/portfolio_2/static/js/main.8fd61a8e.chunk.js"
+  },
+  {
+    "revision": "1cba8cdf0e2b99eeb724",
+    "url": "/portfolio_2/static/js/runtime-main.1ea12f1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio_2/static/media/logo.5d5d9eef.svg"
+  }
+]);
